2e3d13d7f9a74a5ec4923b3245480b0ac7c62925
Armin <datarivers.org@gmail.com>
2023-08-01 08:20:35 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const app = document.getElementById('app');
+
+function Homepage() {
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+ReactDOM.render(<Homepage />, app);۴
---
+++ src/header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+    return(<h1>Develop. Preview. Ship. 🚀</h1>);
+}
