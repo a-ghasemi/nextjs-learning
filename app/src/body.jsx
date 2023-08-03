d9cf8e16168e0269bde9712059691bd00993abf4
Armin Ghasemi <armin.ghasemi@portavita.nl>
2023-08-03 16:02:01 +0200

    uses props

--- app/src/body.jsx
+++ app/src/body.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "./button";
 
 export default function (){
     return(
         <>
             <h2>This is body heading</h2>
             <p>This is body paragraph</p>
+            <Button title="123" value="456"/>
         </>
     );
 }
---
+++ app/src/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ({ title, value }){
+    console.log(title,value);
+    return(
+        <>
+            <button>Title</button>
+        </>
+    );
+}
